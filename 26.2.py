49cf9b8af421825610262cbe428751e796dba132
kairatbek-d <kairatbekdavranbekov@gmail.com>
2022-01-27 10:12:22 +0600

    twenty sixth problem

---
+++ 26.2.py
@@ -0,0 +1,49 @@
+# task:
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such
+# that each unique element appears only once. The relative order of the elements should be kept
+# the same.
+# Since it is impossible to change the length of the array in some languages, you must instead
+# have the result be placed in the first part of the array nums. More formally, if there are
+# k elements after removing the duplicates, then the first k elements of nums should hold the
+# final result. It does not matter what you leave beyond the first k elements.
+# Return k after placing the final result in the first k slots of nums.
+# Do not allocate extra space for another array. You must do this by modifying the input array
+# in-place with O(1) extra memory.
+# Custom Judge:
+
+# The judge will test your solution with the following code:
+
+#     int[] nums = [...]; // Input array
+#     int[] expectedNums = [...]; // The expected answer with correct length
+
+#     int k = removeDuplicates(nums); // Calls your implementation
+
+#     assert k == expectedNums.length;
+#     for (int i = 0; i < k; i++) {
+#         assert nums[i] == expectedNums[i];
+#     }
+# If all assertions pass, then your solution will be accepted.
+
+
+
+# Runtime: 174 ms, faster than 17.42%
+# Memory Usage: 15.6 MB, less than 79.95%
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        newTail = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[newTail]:
+                newTail += 1
+                nums[newTail] = nums[i]
+
+        return newTail + 1
+
+sol = Solution()
+print(sol.removeDuplicates(nums = [0,0,1,1,1,2,2,3,3,4]))
