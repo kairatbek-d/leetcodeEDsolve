f2ea529d8c52c460f335542bb7df532775098e09
kairatbek-d <kairatbekdavranbekov@gmail.com>
2022-02-03 13:23:06 +0600

    thirty second problem

---
+++ 33.2.py
@@ -0,0 +1,44 @@
+# task:
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly rotated at an unknown pivot index
+# k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1],
+# nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated
+# at pivot index 3 and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of
+# target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+
+# Runtime: 81 ms, faster than 9.44%
+# Memory Usage: 14.4 MB, less than 91.62%
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left, right = 0, n - 1
+        if n == 0: return -1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target: return mid
+            
+            if nums[mid] >= nums[left]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+        return -1
+
+sol = Solution()
+print(sol.search(nums = [4,5,6,7,0,1,2], target = 0))
